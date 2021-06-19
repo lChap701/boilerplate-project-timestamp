2732808224de2546d2de501c04fed0ffd5c2d46c
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-06-19 14:07:02 -0500

    My changes

--- server.js
+++ server.js
@@ -18,13 +18,36 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Displays timestamps
+app.get("/api/:date", function(req, res, next) {
+  // Retrieves date from endpoint
+  let { date } = req.params;  
 
+  // Checks if date is in UNIX
+  if (Number.isNaN(Date.parse(date))) {
+    // Saves UNIX
+    req.unix = date;
+
+    // Converts UNIX to UTC
+    req.utc = new Date(+date).toUTCString()
+  } else {
+    // Converts date to UNIX
+    req.unix = new Date(date).getTime();
+
+    // Converts date to UTC
+    req.utc = new Date(date).toUTCString();
+  }
+
+  next();
+}, function(req, res) {
+  // Displays date in UNIX and UTC format as JSON
+  res.json({ unix: req.unix, utc: req.utc });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
